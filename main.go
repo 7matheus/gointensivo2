bfa71389c1a6a96f158c84b8da31a393b951a69f
Matheus Lino <7matheuslino@gmai.com>
2023-03-07 20:46:37 -0300

    Adicionando um reader no canal

--- main.go
+++ main.go
@@ -15,9 +15,18 @@ func task(name string) {
 func main() {
 	canal := make(chan int) // Canal de comunicação entre as threads
 
+	// T2
 	go publish(canal)
 
+	// T3
+	go reader(canal)
+
+	time.Sleep(time.Second * 5)
+}
+
+func reader(canal chan int) {
 	for x := range canal {
+		// value := <-canal
 		fmt.Println(x)
 	}
 }
